27a963ae4f1dd70870cc12f2df9aa410d51c3a32
srikhanth <srikhanth64@gmail.com>
2023-01-27 12:46:16 +0530

    binary change

---
+++ binarytree.py
@@ -0,0 +1,5 @@
+N=int(input("Enter the number of input "))
+lt =[]
+for i in range(0,N):
+    lt.append(int(input("Enter next value ")))
+print(lt.index(int(input("Enter to find value"))))
--- hello.py
+++
@@ -1,3 +0,0 @@
-print("Hello World")
-for cup in range(10):
-    print("I am drinking cup "+ str(cup))
