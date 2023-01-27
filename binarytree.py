d65e0e2ef7335ac9b2c90bb05f4d84dc3e08ee90
srikhanth <srikhanth64@gmail.com>
2023-01-27 12:49:31 +0530

    change in binary

--- binarytree.py
+++ binarytree.py
@@ -2,4 +2,7 @@ N=int(input("Enter the number of input "))
 lt =[]
 for i in range(0,N):
     lt.append(int(input("Enter next value ")))
-print(lt.index(int(input("Enter to find value"))))
+try:
+    print(lt.index(int(input("Enter to find value"))))
+except ValueError:
+    print("-1")
